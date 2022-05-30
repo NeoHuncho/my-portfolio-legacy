bce4c8b3913d56857a10ff6bbef57ed87458d84b
Neo Huncho <william.guinaudie@gmail.com>
2022-05-30 11:53:09 +0200

    changed mobile size

--- styles/mediaQueries.js
+++ styles/mediaQueries.js
@@ -1,11 +1,9 @@
 export const mediaQueries = {
-    mobile: '(max-width: 420px)',
-    nonmobile:'(min-width:421px)',
-    ipad: '(min-width:421px) and (max-width: 811px)',
-    ipadPro: '(min-width:812px) and (max-width: 1024px)',
-    ipadAndIpadPro: '(min-width:421px) and (max-width: 1024px)',
-    nondesktop:'(max-width: 1024px)',
-    desktop: '(min-width: 1025px)',
-  
-    
+  mobile: "(max-width: 460px)",
+  nonmobile: "(min-width:461px)",
+  ipad: "(min-width:461px) and (max-width: 811px)",
+  ipadPro: "(min-width:812px) and (max-width: 1024px)",
+  ipadAndIpadPro: "(min-width:461px) and (max-width: 1024px)",
+  nondesktop: "(max-width: 1024px)",
+  desktop: "(min-width: 1025px)",
 };
