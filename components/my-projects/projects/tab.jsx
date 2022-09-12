e7557f5db89e9980875c161ed68e631369e54c2f
Neo Huncho <william.guinaudie@gmail.com>
2022-09-12 15:08:05 +0200

    changed checkout flow

--- components/my-projects/projects/tab.jsx
+++ components/my-projects/projects/tab.jsx
@@ -10,6 +10,7 @@ import {
   GithubLink,
 } from "./card_styles";
 import { Carousel } from '@mantine/carousel';
+import { Card, Image } from '@mantine/core';
 
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
@@ -36,78 +37,11 @@ function ProjectTab({ items }) {
       align="start">    
         {items.map((item, index) => (
           <Carousel.Slide key={index} style={{borderRadius:'40px'}}>
-            <CardActionArea
-              href={item.link}
-              target="_blank"
-              style={{ textDecoration: "none",  width:'100%' }}
-            >
-              <CardMedia
-                component="img"
-                alt={item.image.name}
-                height="280"
-                image={item.image.image.src}
-                title={item.image.name}
-               
-                
-              />
-
-              <CardContent
-                style={{
-                height: "200px",
-                  display: "flex",
-                  flexDirection: "column",
-                  justifyContent: "space-between",
-                  backgroundColor:'whitesmoke'
-                }}
-              >
-                <TitleSection>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {item.title}
-                    {item.github && (
-                      <GithubLink href={item.github} target="_blank">
-                        <Github src={technologies.github.image.src} />
-                      </GithubLink>
-                    )}
-                  </Typography>
-                </TitleSection>
-
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  style={{}}
-                >
-                  {item.subTitle}
-                </Typography>
-                <TechnologiesSection>
-                  {item.technologies.map((technology, index) => {
-            
-                  return  <Technology
-                      key={index}
-                      title={technology.name}
-                      src={technology.image.src}
-                    />
-                  }
-                  )}
-                </TechnologiesSection>
-
-                <Chip
-                  label={item.status}
-                  style={{
-                    marginTop: 20,
-                    color: "white",
-                    backgroundColor:
-                      item.statusCode === 1
-                        ? blue["700"]
-                        : item.statusCode === 2
-                        ? orange["700"]
-                        : item.statusCode === 3
-                        ? red["700"]
-                        : green["700"],
-                  }}
-                />
-              </CardContent>
-            </CardActionArea>
+        <Card>
+          <Card.Section>
+            <Image alt={item.image.name}height={300} src={item.image.image.src} />
+          </Card.Section>
+        </Card>
           </Carousel.Slide>
         ))}
       </Carousel>
@@ -116,3 +50,76 @@ function ProjectTab({ items }) {
 }
 
 export default ProjectTab;
+
+// <CardActionArea
+// href={item.link}
+// target="_blank"
+// style={{ textDecoration: "none",  width:'100%' }}
+// >
+// <CardMedia
+//   component="img"
+//   alt={item.image.name}
+//   height="280"
+//   image={item.image.image.src}
+//   title={item.image.name}
+ 
+  
+// />
+
+// <CardContent
+//   style={{
+//   height: "200px",
+//     display: "flex",
+//     flexDirection: "column",
+//     justifyContent: "space-between",
+//     backgroundColor:'whitesmoke'
+//   }}
+// >
+//   <TitleSection>
+//     <Typography gutterBottom variant="h5" component="h2">
+//       {item.title}
+//       {item.github && (
+//         <GithubLink href={item.github} target="_blank">
+//           <Github src={technologies.github.image.src} />
+//         </GithubLink>
+//       )}
+//     </Typography>
+//   </TitleSection>
+
+//   <Typography
+//     variant="body2"
+//     color="textSecondary"
+//     component="p"
+//     style={{}}
+//   >
+//     {item.subTitle}
+//   </Typography>
+//   <TechnologiesSection>
+//     {item.technologies.map((technology, index) => {
+
+//     return  <Technology
+//         key={index}
+//         title={technology.name}
+//         src={technology.image.src}
+//       />
+//     }
+//     )}
+//   </TechnologiesSection>
+
+//   <Chip
+//     label={item.status}
+//     style={{
+//       marginTop: 20,
+//       color: "white",
+//       backgroundColor:
+//         item.statusCode === 1
+//           ? blue["700"]
+//           : item.statusCode === 2
+//           ? orange["700"]
+//           : item.statusCode === 3
+//           ? red["700"]
+//           : green["700"],
+//     }}
+//   />
+// </CardContent>
+// </CardActionArea>
